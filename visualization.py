b3eb9c40cc1d3febc6d0804363d067c1c7d2dda9
Nejc Hirci <nhirci@gmail.com>
2023-12-28 19:22:58 +0100

    Fixed path search bug

--- visualization.py
+++ visualization.py
@@ -36,18 +36,20 @@ def build_node_tree(sensors, period=8, grid_size=4):
         # Add siblings to each node
         for node in grid:
             for other_node in grid:
-                if abs(node.x - other_node.x) <= 1 and abs(node.y - other_node.y) <= 1:
+                # Connect only the 4 adjacent nodes
+                if node != other_node and \
+                   abs(node.x - other_node.x) + abs(node.y - other_node.y) == 1:
                     node.siblings.append(other_node)
-        # Try to connect the nodes to the nodes at the previous time step
-        if t == 0:
-            all_nodes.append(grid)
-            continue
-        for node in grid:
-            for prev_node in all_nodes[t-1]:
-                if node.x == prev_node.x and node.y == prev_node.y:
-                    node.parent = prev_node
-                    prev_node.children.append(node)
-                    break
+        # # Try to connect the nodes to the nodes at the previous time step
+        # if t == 0:
+        #     all_nodes.append(grid)
+        #     continue
+        # for node in grid:
+        #     for prev_node in all_nodes[t-1]:
+        #         if node.x == prev_node.x and node.y == prev_node.y:
+        #             node.parent = prev_node
+        #             prev_node.children.append(node)
+        #             break
         all_nodes.append(grid)
 
     return all_nodes, locs
@@ -57,14 +59,18 @@ def find_next_pos(pos, grid, t):
     """Find path between pos and any valid position in grid."""
     # Check if the current position is valid
     if pos in grid:
-        return pos
+        node = grid[grid.index(pos)]
+        return node
 
     # Use BFS to find a valid position
     queue = [pos]
     visited = []
     while queue:
         node = queue.pop(0)
+        # If the node is valid intruder can remain at this position
         if node in grid:
+            # Get node at new time from grid
+            node = grid[grid.index(node)]
             return node
         visited.append(node)
         for neghb in node.siblings:
@@ -112,7 +118,7 @@ def find_path(all_grid):
         path = [pos]
         for t in range(1, len(all_grid)):
             # If the current position is still valid the intruder does not need to move
-            pos = find_next_pos(pos, all_grid[t], t)
+            pos = find_next_pos(pos, all_grid[t], t - 1)
             if pos is None:
                 i += 1
                 break
@@ -120,7 +126,7 @@ def find_path(all_grid):
         # If the path is valid we can return it
         if pos is not None:
             # Check if there exists a path from last position to start position
-            pos = find_next_pos(pos, all_grid[0], 0)
+            pos = find_next_pos(pos, all_grid[0], len(all_grid) - 1)
             if pos is not None:
                 path.append(pos)
                 return path
@@ -136,9 +142,8 @@ if __name__ == "__main__":
     (0, 0) is the top left corner
     (dim, dim) is the bottom right corner
     """
-
-    # Basic Example 1
     """
+    # Basic Example 1
     path = [
         [0, 0],
         [1, 0],
@@ -156,7 +161,6 @@ if __name__ == "__main__":
     node_path = find_path(all_nodes)
     visualize(locs, node_path)
     """
-
     # Example 2
     dim = (8, 8)
     endpoints = [[0, 0], [5, 0]]
